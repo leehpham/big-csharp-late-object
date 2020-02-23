2d2e9ec6c4bd66ab8ea2213a8a6f277f92cc5e67
Hoang Pham <lehoangpham1092@gmail.com>
2020-02-22 21:27:17 -0600

    ...

---
+++ c13-recursion/13-02-problem-solving-thinking-recursively/exercises/P13_06/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace P13_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Find("", "me"));
+            Console.WriteLine(Find("me", ""));
+            Console.WriteLine(Find("b", "h"));
+            Console.WriteLine(Find("b", "b"));
+            Console.WriteLine(Find("Hoang Pham", "g P"));
+            Console.WriteLine(Find("Hoang Pham", "z"));
+        }
+
+        public static bool Find(string text, string str)
+        {
+            if (text.Length < str.Length)
+            {
+                return false;
+            }
+            else if (str.Length == 0 || text.Length == 0)
+            {   
+                return false;
+            }
+            else if (text.Length == 1 && str.Length == 1 && text[0] != str[0]) 
+            {
+                return false;
+            }
+            else 
+            {
+                var textStartStr = text.Substring(0, str.Length);
+                if (textStartStr == str)
+                {
+                    return true;
+                }
+                else
+                {
+                    return Find(text.Substring(1), str);
+                }
+            }
+        }
+    }
+}
