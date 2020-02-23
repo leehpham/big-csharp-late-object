0954a0c33b269b42580f830e57609da5f1503581
Hoang Pham <lehoangpham1092@gmail.com>
2020-02-22 20:47:54 -0600

    ...

---
+++ c13-recursion/13-02-problem-solving-thinking-recursively/exercises/P13_03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P13_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Reverse(""));
+            Console.WriteLine(Reverse("H"));
+            Console.WriteLine(Reverse("I'm handsome!"));
+        }
+
+        public static string Reverse(string text)
+        {
+            if (text.Length <= 1)
+            {
+                return text;
+            }
+            else
+            {
+                var firstChar = text[0];
+                var smallerText = text.Substring(1);
+                var output = Reverse(smallerText) + firstChar;
+                return output;
+            }
+        }
+    }
+}
