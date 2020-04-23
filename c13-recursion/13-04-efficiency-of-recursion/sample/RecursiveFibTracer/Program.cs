4d2a9fb940eac2509c841843ecc973663b9c5d2c
Hoang Pham <lehoangpham1092@gmail.com>
2020-04-22 22:50:05 -0500

    ...

---
+++ c13-recursion/13-04-efficiency-of-recursion/sample/RecursiveFibTracer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace RecursiveFibTracer
+{
+    /// <summary>
+    /// This program computes Fibonacci numbers using a recursive method.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter n: ");
+            int n = Console.Read();
+
+            for (int i = 1; i <= n; i++)
+            {
+
+            }
+        }
+
+        /// <summary>
+        /// Computes a Fibonacci number.
+        /// </summary>
+        /// <param name="n">An integer.</param>
+        /// <returns>The nth Fibonaci number.</returns>
+        public static long Fib(int n)
+        {
+            
+        }
+    }
+}
