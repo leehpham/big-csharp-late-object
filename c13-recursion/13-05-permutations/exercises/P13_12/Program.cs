49762ae768a849659172a9da92151d83cd77ec5a
Hoang Pham <lehoangpham1092@gmail.com>
2020-05-15 09:08:09 -0500

    ...

---
+++ c13-recursion/13-05-permutations/exercises/P13_12/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P13_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static List<>
+    }
+}
