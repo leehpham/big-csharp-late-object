59590ef837de0c2f2843102f71c1020339a9b9ac
Hoang Pham <lehoangpham1092@gmail.com>
2020-06-10 20:07:32 -0500

    ...

---
+++ c13-recursion/13-06-mutual-recursion/samples/ExpressionCalculator/Evaluator.cs
@@ -0,0 +1,49 @@
+using System;
+
+namespace ExpressionCalculator
+{
+    /// <summary>
+    /// A class that can computes the value of an arithmetic expression.
+    /// </summary>
+    public class Evaluator
+    {
+        private ExpressionTokenizer tokenizer;
+
+        /// <summary>
+        /// Constructs an evaluator.
+        /// </summary>
+        /// <param name="anExpression">a string containing the expression to be evaluated.</param>
+        public Evaluator(string anExpression)
+        {
+            tokenizer = new ExpressionTokenizer(anExpression);
+        }
+
+        /// <summary>
+        /// Evaluates the expression.
+        /// </summary>
+        /// <returns>The value of the expression.</returns>
+        public int GetExpressionValue()
+        {
+            int value = GetTermValue();
+            bool done = false;
+        }
+
+        /// <summary>
+        /// Evaluates the next term found in the expression.
+        /// </summary>
+        /// <returns>The value of the term.</returns>
+        public int GetTermValue()
+        {
+            int value = GetFactorValue();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public int GetFactorValue()
+        {
+
+        }
+    }
+}
---
+++ c13-recursion/13-06-mutual-recursion/samples/ExpressionCalculator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ExpressionCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tokenizer = new ExpressionTokenizer("123+45");
+            Console.WriteLine(tokenizer.NextToken());
+        }
+    }
+}
