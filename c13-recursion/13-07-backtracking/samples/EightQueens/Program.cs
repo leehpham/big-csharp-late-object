534a33dbfcce8e716cdae870c6d92b00c0c13673
Hoang Pham <lehoangpham1092@gmail.com>
2020-09-30 22:41:12 -0500

    ...

---
+++ c13-recursion/13-07-backtracking/samples/EightQueens/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace EightQueens
+{
+    /// <summary>
+    /// This class solves the eight queens problem using backtracking.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solve(new PartialSolution(0));
+        }
+
+        public static void Solve(PartialSolution sol)
+        {
+            int exam = sol.Examine();
+            if (exam == PartialSolution.ACCEPT)
+            {
+                Console.WriteLine(sol);
+            }
+            else if (exam != PartialSolution.ABANDON)
+            {
+                foreach (PartialSolution p in sol.Extend())
+                {
+                    Solve(p);
+                }
+            }
+        }
+    }
+}
